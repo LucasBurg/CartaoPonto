308a043f08c76093b5e3db5228c0d2820862afef
Lucas Mota <lucas.mota@controller.education>
2017-12-08 16:50:33 -0200

    Adição do mudulo funci_list

---
+++ lib/funci_list/funci_list_component.dart
@@ -0,0 +1,10 @@
+import 'package:angular/angular.dart';
+import 'package:angular_router/angular_router.dart';
+
+@Component(
+  selector: 'funci-list',
+  templateUrl: 'funci_list_component.html',
+)
+class FunciListComponent {
+  
+}
