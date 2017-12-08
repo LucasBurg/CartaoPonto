c3743ca67d6bd83eb9013f76ce108a5444c5120c
Lucas Mota <lucas.mota@controller.education>
2017-12-08 17:19:18 -0200

    Implementação do modulo funci_list para o modulo funci

--- lib/funci/funci_component.dart
+++ lib/funci/funci_component.dart
@@ -1,9 +1,12 @@
 import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
 
+import '../funci_list/funci_list_component.dart';
+
 @Component(
   selector: 'funci',
   templateUrl: 'funci_component.html',
+  directives: const [FunciListComponent],
 )
 class FunciComponent {
   
--- lib/funci_list/funci_list_component.dart
+++ lib/funci_list/funci_list_component.dart
@@ -4,6 +4,7 @@ import 'package:angular_router/angular_router.dart';
 @Component(
   selector: 'funci-list',
   templateUrl: 'funci_list_component.html',
+  styleUrls: const ['table.css']
 )
 class FunciListComponent {
   
