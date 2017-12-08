9b7ea51568d7f0bcd0a4adae00f1a019c46271de
Lucas Mota <lucas.mota@controller.education>
2017-12-08 16:09:39 -0200

    Criado o componente funci

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,8 +1,11 @@
 import 'package:angular/angular.dart';
 
+import 'funci/funci_component.dart';
+
 @Component(
   selector: 'my-app',
-  template: '<h1>Hello {{name}}</h1>',
+  template: '<h1>Hello {{name}}</h1> <funci></funci>',
+  directives: const [FunciComponent]
 )
 class AppComponent {
   var name = 'Angular';
---
+++ lib/funci/funci_component.dart
@@ -0,0 +1,9 @@
+import 'package:angular/angular.dart';
+
+@Component(
+  selector: 'funci',
+  template: 'ola mundo',
+)
+class FunciComponent {
+  
+}
